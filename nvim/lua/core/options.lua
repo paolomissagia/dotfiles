8252605bb4179b2f1782e1646a24f17fb7ee3e5b
Paolo Missagia <login@paolomissagia.com>
2024-05-31 09:45:24 +0100

    updated tab spacing and word wrap

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -3,9 +3,9 @@ vim.opt.relativenumber = true
 vim.opt.number = true
 
 -- indentation
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
@@ -32,3 +32,6 @@ vim.opt.scrolloff = 8
 -- window manager
 vim.opt.splitbelow = true
 vim.opt.splitright = true
+
+-- word wrap
+vim.o.wrap = false
