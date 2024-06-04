f2b82a4b49f84efe910925aa9ae61f67173469c3
Paolo Missagia <login@paolomissagia.com>
2024-06-05 00:16:01 +0100

    cleaning files

--- nvim/lua/plugins/nvim-lint.lua
+++ nvim/lua/plugins/nvim-lint.lua
@@ -17,7 +17,7 @@ return {
 		})
 
 		-- Set pylint to work in virtualenv
-		require("lint").linters.pylint.cmd = "python"
-		require("lint").linters.pylint.args = { "-m", "pylint", "-f", "json" }
+		lint.linters.pylint.cmd = "python"
+		lint.linters.pylint.args = { "-m", "pylint", "-f", "json" }
 	end,
 }
