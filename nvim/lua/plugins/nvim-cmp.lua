eafca88914c0ac21791d19b9c01cf9c2b0c3241d
Paolo Missagia <login@paolomissagia.com>
2024-06-05 09:30:46 +0100

    reverted bordered for autocompletion

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -8,6 +8,10 @@ return {
 		local cmp = require("cmp")
 
 		cmp.setup({
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
 			mapping = cmp.mapping.preset.insert({
 				["<C-n>"] = cmp.mapping.select_next_item(),
 				["<C-p>"] = cmp.mapping.select_prev_item(),
