ac1a7571a20cc68caf785dacefa6c4cc5464888f
Paolo Missagia <login@paolomissagia.com>
2024-07-17 17:56:12 +0100

    added panes navigation

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,9 +2,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- navigation
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+-- panes navigation
+vim.keymap.set("n", "<c-h>", "<c-w>h")
+vim.keymap.set("n", "<c-j>", "<c-w>j")
+vim.keymap.set("n", "<c-k>", "<c-w>k")
+vim.keymap.set("n", "<c-l>", "<c-w>l")
 
 -- search
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
