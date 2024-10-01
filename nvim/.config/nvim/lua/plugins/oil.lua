d59f6ed45f5e97f6ea0152a18f391f7c15c381ec
Paolo Missagia <login@paolomissagia.com>
2024-10-01 16:19:38 +0100

    added oil

---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+	"stevearc/oil.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		vim.keymap.set("n", "<leader>e", function()
+			if vim.bo.filetype == "oil" then
+				require("oil").close()
+			else
+				require("oil").open()
+			end
+		end, { desc = "File navigation" }),
+	},
+}
