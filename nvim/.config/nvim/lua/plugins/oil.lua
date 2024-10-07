655789205e48e6804149567286c4437d37c29d56
Paolo Missagia <login@paolomissagia.com>
2024-10-07 16:20:19 +0100

    show hidden files on oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,13 +1,21 @@
 return {
 	"stevearc/oil.nvim",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		vim.keymap.set("n", "<leader>e", function()
-			if vim.bo.filetype == "oil" then
-				require("oil").close()
-			else
-				require("oil").open()
-			end
-		end, { desc = "File explorer" }),
-	},
+	config = function()
+		local oil = require("oil")
+
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+			},
+
+			vim.keymap.set("n", "<leader>e", function()
+				if vim.bo.filetype == "oil" then
+					oil.close()
+				else
+					oil.open()
+				end
+			end, { desc = "File explorer" }),
+		})
+	end,
 }
