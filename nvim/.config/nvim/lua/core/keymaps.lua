f8a1a9acf52663306b8ab7892312aedf0864c39d
Paolo Missagia <login@paolomissagia.com>
2024-10-07 16:19:54 +0100

    removed unecessary config

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,11 +2,5 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- panes navigation
-vim.keymap.set("n", "<c-h>", "<c-w>h")
-vim.keymap.set("n", "<c-j>", "<c-w>j")
-vim.keymap.set("n", "<c-k>", "<c-w>k")
-vim.keymap.set("n", "<c-l>", "<c-w>l")
-
 -- search
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
