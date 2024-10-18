4fc73b7477adbb6c3a727b4f4d7e69b05d15c950
Paolo Missagia <login@paolomissagia.com>
2024-10-18 06:49:05 +0100

    removed kitty and moved to wezterm on linux

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,11 +4,14 @@ local config = wezterm.config_builder()
 
 config.color_scheme = "Tokyo Night Moon"
 config.enable_tab_bar = false
-config.font_size = 9.5
-config.default_domain = "WSL:Fedora-40"
-config.default_cmd = "~"
+config.font_size = 10
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	config.default_domain = "WSL:Fedora-40"
+	config.default_cmd = "~"
+end
 
 config.window_padding = {
 	left = 0,
