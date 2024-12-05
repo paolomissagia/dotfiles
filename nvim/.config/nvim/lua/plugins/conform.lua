324498fb373174f07a44d6d34a3233333ff0de5c
Paolo Missagia <login@paolomissagia.com>
2024-12-05 22:47:14 +0000

    trying new stuff

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -7,8 +7,8 @@ return {
 		require("mason-tool-installer").setup({
 			ensure_installed = {
 				"stylua",
-				"black",
-				"rubocop",
+				"autopep8",
+				"standardrb",
 				"prettierd",
 			},
 		})
@@ -16,8 +16,8 @@ return {
 		require("conform").setup({
 			formatters_by_ft = {
 				lua = { "stylua" },
-				python = { "black" },
-				ruby = { "rubocop" },
+				python = { "autopep8" },
+				ruby = { "standardrb" },
 				go = { "gofmt" },
 				javascript = { "prettierd" },
 				javascriptreact = { "prettierd" },
--- nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,8 +12,8 @@ return {
 		mason_lspconfig.setup({
 			ensure_installed = {
 				"lua_ls",
-				"pyright",
-				"solargraph",
+				"pylsp",
+				"ruby_lsp",
 				"gopls",
 				"ts_ls",
 				"html",
