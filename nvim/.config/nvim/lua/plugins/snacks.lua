8903dedc89015e1148c2328c9ff48247d087b76d
Paolo Missagia <login@paolomissagia.com>
2024-12-09 12:12:34 +0000

    simplified dashboard

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -3,43 +3,6 @@ return {
 	priority = 1000,
 	lazy = false,
 	opts = {
-		dashboard = {
-			preset = {
-				keys = {
-					{ icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-					{
-						icon = " ",
-						key = "r",
-						desc = "Recent Files",
-						action = ":lua Snacks.dashboard.pick('oldfiles')",
-					},
-					{
-						icon = " ",
-						key = "l",
-						desc = "Leetcode",
-						action = ":Leet",
-					},
-					{
-						icon = "󱌣 ",
-						key = "m",
-						desc = "Mason",
-						action = ":Mason",
-					},
-					{
-						icon = "󰒲 ",
-						key = "L",
-						desc = "Lazy",
-						action = ":Lazy",
-					},
-					{
-						icon = " ",
-						key = "c",
-						desc = "Config",
-						action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})",
-					},
-					{ icon = " ", key = "q", desc = "Quit", action = ":qa" },
-				},
-			},
-		},
+		dashboard = { enabled = true },
 	},
 }
