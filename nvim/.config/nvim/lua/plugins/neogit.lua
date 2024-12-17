7069a59b4ab99ee27819e555cea0d7e24692aa5e
Paolo Missagia <login@paolomissagia.com>
2024-12-17 22:23:46 +0100

    updated wezterm config for windows and other stuff

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -1,7 +1,6 @@
 return {
 	"NeogitOrg/neogit",
 	dependencies = {
-		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
 		"sindrets/diffview.nvim",
 	},
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,18 +2,14 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
+config.font_size = 10
 config.color_scheme = "Tokyo Night Moon"
 config.enable_tab_bar = false
-config.font_size = 10.5
+config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.warn_about_missing_glyphs = false
-
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	config.font_size = 10
-	config.window_decorations = "RESIZE"
-	config.default_domain = "WSL:Fedora-41"
-	config.default_cwd = "~"
-end
+config.default_domain = "WSL:Ubuntu-24.04"
+config.default_cwd = "~"
 
 config.window_padding = {
 	left = 0,
