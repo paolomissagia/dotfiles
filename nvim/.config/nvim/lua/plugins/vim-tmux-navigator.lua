b3b76a42eb1b81e48da53acbe80fc676a998ca29
Paolo Missagia <login@paolomissagia.com>
2024-12-18 22:44:55 +0100

    updated setup of tmux vim-tmux-navigator

--- nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
+++ nvim/.config/nvim/lua/plugins/vim-tmux-navigator.lua
@@ -1,9 +1,9 @@
 return {
 	"christoomey/vim-tmux-navigator",
-	config = function()
-		vim.keymap.set("n", "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
-		vim.keymap.set("n", "<C-j>", "<cmd>TmuxNavigateDown<cr>")
-		vim.keymap.set("n", "<C-k>", "<cmd>TmuxNavigateUp<cr>")
-		vim.keymap.set("n", "<C-l>", "<cmd>TmuxNavigateRight<cr>")
-	end,
+	keys = {
+		{ "<c-h>", "<cmd>TmuxNavigateLeft<cr>" },
+		{ "<c-j>", "<cmd>TmuxNavigateDown<cr>" },
+		{ "<c-k>", "<cmd>TmuxNavigateUp<cr>" },
+		{ "<c-l>", "<cmd>TmuxNavigateRight<cr>" },
+	},
 }
