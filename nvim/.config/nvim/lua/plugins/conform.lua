02f913099c86e5b0c80e00145eb3d33b8f2739d5
Paolo Missagia <login@paolomissagia.com>
2024-12-18 23:17:08 +0100

    modernized conform setup

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,23 +1,21 @@
 return {
 	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "autopep8" },
-				ruby = { "standardrb" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				html = { "prettierd" },
-				css = { "prettierd" },
-			},
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "autopep8" },
+			ruby = { "standardrb" },
+			javascript = { "prettierd" },
+			javascriptreact = { "prettierd" },
+			typescript = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			html = { "prettierd" },
+			css = { "prettierd" },
+		},
 
-			format_on_save = {
-				timeout_ms = 2500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+		format_on_save = {
+			timeout_ms = 2500,
+			lsp_fallback = true,
+		},
+	},
 }
