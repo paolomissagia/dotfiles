c5e7514e2fd2f388264935cbdf11fe799843ea59
Paolo Missagia <login@paolomissagia.com>
2024-12-18 23:11:25 +0100

    moved to fzf (follow folke)

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -2,4 +2,8 @@ return {
 	"ibhagwan/fzf-lua",
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = true,
+	keys = {
+		{ "<leader>ff", "<cmd>FzfLua files<cr>" },
+		{ "<leader>fg", "<cmd>FzfLua live_grep<cr>" },
+	},
 }
--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -2,7 +2,6 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope.nvim",
 	},
 	config = true,
 	keys = {
