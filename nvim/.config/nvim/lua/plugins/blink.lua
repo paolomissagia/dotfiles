c12152feb0f5e84b99d91c3a2f7b3c4c1c77aaf0
Paolo Missagia <login@paolomissagia.com>
2024-12-20 19:15:59 +0100

    added subdependencies and config for blink

--- nvim/.config/nvim/lua/plugins/blink.lua
+++ nvim/.config/nvim/lua/plugins/blink.lua
@@ -1,14 +1,37 @@
 return {
 	"saghen/blink.cmp",
 	version = "v0.*",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
 	config = function()
 		local blink = require("blink.cmp")
 
 		blink.setup({
-			keymap = { preset = "default" },
+			keymap = {
+				preset = "default",
+			},
 			appearance = {
 				use_nvim_cmp_as_default = true,
 				nerd_font_variant = "mono",
+			},
+			sources = {
+				default = {
+					"lsp",
+					"path",
+					"snippets",
+					"buffer",
+				},
+			},
+			signature = {
+				enabled = false,
+			},
+			completion = {
+				accept = {
+					auto_brackets = {
+						enabled = true,
+					},
+				},
 			},
 		})
 	end,
--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -2,6 +2,8 @@ return {
 	"NeogitOrg/neogit",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
+		"sindrets/diffview.nvim",
+		"ibhagwan/fzf-lua",
 	},
 	config = function()
 		local neogit = require("neogit")
