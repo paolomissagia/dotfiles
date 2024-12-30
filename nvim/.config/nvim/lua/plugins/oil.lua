f9d2de0e38c99454a84f50d527aa4551e10ea36c
Paolo Missagia <login@paolomissagia.com>
2024-12-30 10:05:39 +0100

    added hidden files to oil

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -6,7 +6,11 @@ return {
 	config = function()
 		local oil = require("oil")
 
-		oil.setup({})
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+			},
+		})
 
 		vim.keymap.set("n", "<leader>e", "<cmd>Oil<CR>")
 	end,
