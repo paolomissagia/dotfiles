5799f92d9587ff13b688663a0c1802413262d707
Paolo Missagia <login@paolomissagia.com>
2025-01-06 18:45:54 +0000

    moved to regular prettier and increased timeout on starship

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,23 +1,23 @@
 return {
-	"stevearc/conform.nvim",
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				lua = { "stylua" },
-				python = { "autopep8" },
-				ruby = { "standardrb" },
-				javascript = { "prettierd" },
-				javascriptreact = { "prettierd" },
-				typescript = { "prettierd" },
-				typescriptreact = { "prettierd" },
-				html = { "prettierd" },
-				css = { "prettierd" },
-			},
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        lua = { "stylua" },
+        python = { "autopep8" },
+        ruby = { "standardrb" },
+        javascript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        html = { "prettier" },
+        css = { "prettier" },
+      },
 
-			format_on_save = {
-				timeout_ms = 2500,
-				lsp_fallback = true,
-			},
-		})
-	end,
+      format_on_save = {
+        timeout_ms = 2500,
+        lsp_fallback = true,
+      },
+    })
+  end,
 }
