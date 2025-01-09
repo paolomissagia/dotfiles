5581fdebf02841811eba6ac497ce8e69877c6f5b
Paolo Missagia <login@paolomissagia.com>
2025-01-09 19:09:03 +0000

    loaded fzf ext on telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,7 +6,9 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
-    require("telescope").setup({
+    local telescope = require("telescope")
+
+    telescope.setup({
       defaults = {
         file_ignore_patterns = {
           "vendor",
@@ -19,6 +21,8 @@ return {
         }
       }
     })
+
+    telescope.load_extension("fzf")
 
     local builtin = require("telescope.builtin")
 
