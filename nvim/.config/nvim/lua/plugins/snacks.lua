161f46c74ded754419271bf8429f8f61b263ae18
Paolo Missagia <login@paolomissagia.com>
2025-01-18 17:16:36 +0000

    replaced telescope with snacks picker

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -7,6 +7,29 @@ return {
 
     snacks.setup({
       dashboard = { enabled = true },
+      picker = { enabled = true }
     })
+
+    local ignore = {
+      "/vendor",
+      "/cypress",
+      "/deploy",
+      "/build",
+      "/android",
+      "/ios",
+      "/api-mock"
+    }
+
+    vim.keymap.set("n", "<leader>ff", function()
+      Snacks.picker.files({
+        exclude = ignore
+      })
+    end)
+
+    vim.keymap.set("n", "<leader>fg", function()
+      Snacks.picker.grep({
+        exclude = ignore
+      })
+    end)
   end,
 }
