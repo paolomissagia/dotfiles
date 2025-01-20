7166a82802e4b5a880142554c43e448272f7000d
Paolo Missagia <login@paolomissagia.com>
2025-01-20 10:03:59 +0000

    improved config for snacks

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -2,34 +2,26 @@ return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
-  config = function()
-    local snacks = require("snacks")
+  opts = {
+    dashboard = { enabled = true },
+    picker = { enabled = true },
+    lazygit = { enabled = true },
 
-    -- config
-    snacks.setup({
-      dashboard = { enabled = true },
-      picker = { enabled = true },
-      lazygit = { enabled = true },
-
-      styles = {
-        lazygit = {
-          width = 0,
-          height = 0
+    styles = {
+      lazygit = {
+        width = 0,
+        height = 0
+      },
+      terminal = {
+        keys = {
+          term_normal = false
         }
       }
-    })
-
-    -- keymaps
-    vim.keymap.set("n", "<leader>lg", function()
-      Snacks.lazygit()
-    end)
-
-    vim.keymap.set("n", "<leader>ff", function()
-      Snacks.picker.files()
-    end)
-
-    vim.keymap.set("n", "<leader>fg", function()
-      Snacks.picker.grep()
-    end)
-  end,
+    }
+  },
+  keys = {
+    { "<leader>lg", function() Snacks.lazygit() end },
+    { "<leader>ff", function() Snacks.picker.files() end },
+    { "<leader>fg", function() Snacks.picker.grep() end },
+  }
 }
