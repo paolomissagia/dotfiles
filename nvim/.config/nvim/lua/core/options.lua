13afa596a1d2c847803fe745956ee7a9c619625f
Paolo Missagia <hello@paolomissagia.com>
2025-08-27 16:05:30 +0200

    feat: updated tab to 4 identation

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -4,8 +4,8 @@ vim.opt.relativenumber = true
 vim.opt.cursorline = true
 
 -- indentation
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
